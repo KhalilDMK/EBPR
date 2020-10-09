08044eb833614f1c6e056b89c9768a7e029c0711
KhalilDMK <khalildamak93@gmail.com>
2020-10-08 21:31:40 -0400

    Defined unbiased estimators.

--- Code/engine_EBPR.py
+++ Code/engine_EBPR.py
@@ -12,14 +12,33 @@ class Engine(object):
         self._metron = MetronAtK(top_k=config['top_k'], loo_eval=self.config['loo_eval'])
         self.opt = use_optimizer(self.model, config)
 
-    def train_single_batch_EBPR(self, users, pos_items, neg_items, ratings, explainability_matrix):
-        assert hasattr(self, 'model'), 'Please specify the exact model !'
+    def train_single_batch_EBPR(self, users, pos_items, neg_items, ratings, explainability_matrix, popularity_vector, neighborhood):
+        assert hasattr(self, 'model'), 'Please specify the exact model!'
+        assert self.config['model'] in ['BPR', 'UBPR', 'EBPR', 'pUEBPR', 'UEBPR'], 'Please specify the right model!'
         if self.config['use_cuda'] is True:
             users, pos_items, neg_items, ratings = users.cuda(), pos_items.cuda(), neg_items.cuda(), ratings.cuda()
         self.opt.zero_grad()
         pos_prediction, neg_prediction = self.model(users, pos_items, neg_items)
-        loss = - ((pos_prediction - neg_prediction).sigmoid().log() * explainability_matrix[users, pos_items] * (1 - explainability_matrix[users, neg_items])).sum()
-        #loss = - (pos_prediction - neg_prediction).sigmoid().log().sum()
+        if self.config['model'] == 'BPR':
+            loss = - (pos_prediction - neg_prediction).sigmoid().log().sum()
+        elif self.config['model'] == 'UBPR':
+            loss = - ((pos_prediction - neg_prediction).sigmoid().log() * (1 / popularity_vector[pos_items]) * (
+                        1 - 1 / popularity_vector[neg_items])).sum()
+        elif self.config['model'] == 'EBPR':
+            loss = - ((pos_prediction - neg_prediction).sigmoid().log() * explainability_matrix[users, pos_items] * (
+                        1 - explainability_matrix[users, neg_items])).sum()
+        elif self.config['model'] == 'pUEBPR':
+            loss = - ((pos_prediction - neg_prediction).sigmoid().log() * (1 / popularity_vector[pos_items]) * (
+                        1 - 1 / popularity_vector[neg_items]) * explainability_matrix[users, pos_items] * (
+                                  1 - explainability_matrix[users, neg_items])).sum()
+        elif self.config['model'] == 'UEBPR':
+            loss = - ((pos_prediction - neg_prediction).sigmoid().log() * (1 / popularity_vector[pos_items]) * (
+                        1 - 1 / popularity_vector[neg_items]) * explainability_matrix[users, pos_items] /
+                      popularity_vector[neighborhood[pos_items].flatten()].view(len(pos_items),
+                                                                                self.config['neighborhood']).mean(1) * (
+                                  1 - explainability_matrix[users, neg_items] / popularity_vector[
+                              neighborhood[pos_items].flatten()].view(len(pos_items), self.config['neighborhood']).mean(
+                              1))).sum()
         if self.config['l2_regularization'] > 0:
             l2_reg = 0
             for param in self.model.parameters():
@@ -30,18 +49,20 @@ class Engine(object):
         loss = loss.item()
         return loss
 
-    def train_an_epoch(self, train_loader, explainability_matrix, epoch_id):
-        assert hasattr(self, 'model'), 'Please specify the exact model !'
+    def train_an_epoch(self, train_loader, explainability_matrix, popularity_vector, neighborhood, epoch_id):
+        assert hasattr(self, 'model'), 'Please specify the exact model!'
         self.model.train()
         if self.config['use_cuda'] is True:
             explainability_matrix = torch.from_numpy(explainability_matrix).float().cuda()
+            popularity_vector = torch.from_numpy(popularity_vector).float().cuda()
+            neighborhood = torch.from_numpy(neighborhood).cuda()
         total_loss = 0
         bar = pyprind.ProgBar(len(train_loader))
         for batch_id, batch in enumerate(train_loader):
             bar.update()
             assert isinstance(batch[0], torch.LongTensor)
             user, pos_item, neg_item, rating = batch[0], batch[1], batch[2], batch[3]
-            loss = self.train_single_batch_EBPR(user, pos_item, neg_item, rating, explainability_matrix)
+            loss = self.train_single_batch_EBPR(user, pos_item, neg_item, rating, explainability_matrix, popularity_vector, neighborhood)
             total_loss += loss
 
     def evaluate(self, evaluate_data, explainability_matrix, epoch_id):
@@ -78,7 +99,7 @@ class Engine(object):
                 hr, ndcg, mep, wmep = self._metron.cal_hit_ratio_loo(), self._metron.cal_ndcg_loo(), self._metron.cal_mep(explainability_matrix, theta=0), self._metron.cal_weighted_mep(explainability_matrix, theta=0)
                 print('Evaluating Epoch {}: NDCG@{} = {:.4f}, HR@{} = {:.4f}, MEP@{} = {:.4f}, WMEP@{} = {:.4f}'.format(epoch_id, self.config['top_k'],
                                                                                      ndcg, self.config['top_k'], hr, self.config['top_k'], mep, self.config['top_k'], wmep))
-                return ndcg, hr, mep
+                return ndcg, hr, mep, wmep
             else:
                 for batch_id, batch in enumerate(evaluate_data):
                     test_users, test_items, test_output = batch[0], batch[1], batch[2]
@@ -95,31 +116,35 @@ class Engine(object):
             self._metron.subjects = [test_users_eval, test_items_eval, test_output_eval, test_scores_eval]
             map, ndcg, mep, wmep = self._metron.cal_map_at_k(), self._metron.cal_ndcg(), self._metron.cal_mep(explainability_matrix, theta=0), self._metron.cal_weighted_mep(explainability_matrix, theta=0)
             print('Evaluating Epoch {}: MAP@{} = {:.4f}, NDCG@{} = {:.4f}, MEP@{} = {:.4f}, WMEP@{} = {:.4f}'.format(epoch_id, self.config['top_k'], map, self.config['top_k'], ndcg, self.config['top_k'], mep, self.config['top_k'], wmep))
-            return map, ndcg, mep
+            return map, ndcg, mep, wmep
 
-    def save_explicit(self, alias, epoch_id, map, ndcg, num_epoch, best_model, best_performance):
+    def save_explicit(self, epoch_id, map, ndcg, mep, wmep, num_epoch, best_model, best_performance):
         assert hasattr(self, 'model'), 'Please specify the exact model !'
         if ndcg > best_performance[1]:
             best_performance[0] = map
             best_performance[1] = ndcg
-            best_performance[2] = epoch_id
+            best_performance[2] = mep
+            best_performance[3] = wmep
+            best_performance[4] = epoch_id
             best_model = self.model
         if epoch_id == num_epoch - 1:
-            alias += '_batchsize' + str(self.config['batch_size']) + '_opt_' + str(self.config['optimizer']) + '_lr_' + str(self.config['lr']) + '_latent_' + str(self.config['num_latent']) + '_l2reg_' + str(self.config['l2_regularization'])
-            model_dir = self.config['model_dir_explicit'].format(alias, best_performance[2], self.config['top_k'], best_performance[0], self.config['top_k'], best_performance[1])
+            alias = self.config['model'] + '_' + self.config['dataset'] + '_batchsize_' + str(self.config['batch_size']) + '_opt_' + str(self.config['optimizer']) + '_lr_' + str(self.config['lr']) + '_latent_' + str(self.config['num_latent']) + '_l2reg_' + str(self.config['l2_regularization'])
+            model_dir = self.config['model_dir_explicit'].format(alias, best_performance[4], self.config['top_k'], best_performance[0], self.config['top_k'], best_performance[1], self.config['top_k'], best_performance[2], self.config['top_k'], best_performance[3])
             save_checkpoint(best_model, model_dir)
         return best_model, best_performance
 
-    def save_implicit(self, alias, epoch_id, ndcg, hr, num_epoch, best_model, best_performance):
+    def save_implicit(self, epoch_id, ndcg, hr, mep, wmep, num_epoch, best_model, best_performance):
         assert hasattr(self, 'model'), 'Please specify the exact model !'
         if ndcg > best_performance[0]:
             best_performance[0] = ndcg
             best_performance[1] = hr
-            best_performance[2] = epoch_id
+            best_performance[2] = mep
+            best_performance[3] = wmep
+            best_performance[4] = epoch_id
             best_model = self.model
         if epoch_id == num_epoch - 1:
-            alias += '_batchsize' + str(self.config['batch_size']) + '_opt_' + str(self.config['optimizer']) + '_lr_' + str(self.config['lr']) + '_latent_' + str(self.config['num_latent']) + '_l2reg_' + str(self.config['l2_regularization'])
-            model_dir = self.config['model_dir_implicit'].format(alias, best_performance[2], self.config['top_k'], best_performance[1], self.config['top_k'], best_performance[0])
+            alias = self.config['model'] + '_' + self.config['dataset'] + '_batchsize_' + str(self.config['batch_size']) + '_opt_' + str(self.config['optimizer']) + '_lr_' + str(self.config['lr']) + '_latent_' + str(self.config['num_latent']) + '_l2reg_' + str(self.config['l2_regularization'])
+            model_dir = self.config['model_dir_implicit'].format(alias, best_performance[4], self.config['top_k'], best_performance[1], self.config['top_k'], best_performance[0], self.config['top_k'], best_performance[2], self.config['top_k'], best_performance[3])
             save_checkpoint(best_model, model_dir)
         return best_model, best_performance
 
